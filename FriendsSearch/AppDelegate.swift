174155563e2bbd69a5a9fef3483ee42e12663ae9
大嶋広也 <ooshimahiroya@ooshimahironaris-MacBook-Air.local>
2017-02-12 23:20:47 +0900

    20170212

--- FriendsSearch/AppDelegate.swift
+++ FriendsSearch/AppDelegate.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    
+    func application(application: UIApplication,didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    func application(application: UIApplication,openURL url: NSURL,sourceApplication: String?,annotation: AnyObject?) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url as URL!, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
--- FriendsSearch/ViewController.swift
+++ FriendsSearch/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //ログインボタンが押された時の処理。Facebookの認証とその結果を取得する
+    func loginButton(_ loginButton: FBSDKLoginButton!,didCompleteWith
+        result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            //エラー処理
+        } else if result.isCancelled {
+            //キャンセルされた時
+        } else {
+            //必要な情報が取れていることを確認(今回はemail必須)
+            if result.grantedPermissions.contains("email")
+            {
+                // 次の画面に遷移（後で）
+            }
+        }
+    }
+    
+    //ログアウトボタンが押された時の処理
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
 }
+
+    override func viewDidAppear(_ animated: Bool) {
+        if (FBSDKAccessToken.current() != nil) {
+            print("User Already Logged In")
+            //後で既にログインしていた場合の処理（メイン画面へ遷移）を書く
+        } else {
+            print("User not Logged In")
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+}
+}
+
+
 
